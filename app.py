604db6b428a1fe31ea3bb0feced468f62d198eb8
vaibhav vala <vaibhavvala10@gmail.com>
2026-06-18 22:12:37 +0530

    initial commit

--- app.py
+++ app.py
@@ -62,4 +62,5 @@ if st.button('Predict Price'):
     query = np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
 
     query = query.reshape(1,12)
-    st.title("The predicted price is " + str(int(np.exp(pipe.predict(query)[0]))))
+    price = int(np.exp(pipe.predict(query)[0]))
+    st.success(f"The predicted price is ₹{price:,}")
